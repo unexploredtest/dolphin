e3ce05dfebdcbb2cbeba5676d33de3a94f78d09a
unexploredtest <unexploredtest@tutanota.com>
2025-06-28 20:37:28 +0330

    Fix bundle

--- BuildMacOSUniversalBinary.py
+++ BuildMacOSUniversalBinary.py
@@ -331,9 +331,10 @@ def build(config):
 
     # Source binary trees to merge together
     src_app0 = ARCHITECTURES[0]+"/Binaries/"
-    src_app1 = ARCHITECTURES[1]+"/Binaries/"
+    # src_app1 = ARCHITECTURES[1]+"/Binaries/"
 
-    recursive_merge_binaries(src_app0, src_app1, dst_app)
+    # recursive_merge_binaries(src_app0, src_app1, dst_app)
+    dst_app = src_app0
     
     if config["autoupdate"]:
         subprocess.check_call([
